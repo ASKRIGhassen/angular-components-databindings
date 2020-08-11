20333537005400d950a37b2a13739918f028ca9c
ASKRIGhassen <gh.askri@gmail.com>
2020-08-11 22:29:13 +0200

    last commit

--- databings-demo/src/app/server-element/server-element.component.ts
+++ databings-demo/src/app/server-element/server-element.component.ts
@@ -1,4 +1,14 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +16,25 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // Native , None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, AfterContentInit {
   @Input() element: { type: string, name: string, content: string };
-
+  @ContentChild('contentParagraph', {static: true}) contentParagraph: ElementRef;
   constructor() {
+    console.log('constructor called!');
   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('aftercontentinit called !');
+    console.log('content is :' + this.contentParagraph.nativeElement.textContent);
   }
 
 }
